151b87725a25646a51017f14d48d2deded88f933
Abdul <awsayed@alaska.edu>
2019-06-07 15:27:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,24 @@
+require "pry"
+
 class Song
   @@count = 0
   @@artists = []
   @@genres = []
 
+  # returns the number of songs per genre
+  # {"rap" => 5, "rock" => 1, "country" => 3}
   @@genre_count = {}
+
+  # returns the number of songs per artist
+  #{"Beyonce" => 17, "Jay-Z" => 40}
   @@artist_count = {}
 
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
-    @name = name, @artist = artist, @genre = genre
+    @name = name
+    @artist = artist
+    @genre = genre
     @@count += 1
     @@artists << @artist
     @@genres << @genre
@@ -26,5 +35,11 @@ class Song
   def self.genres
     @@genres
   end
-  
 end
+
+lucifer = Song.new("Lucifer", "Jay-Z", "rap")
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+hit_me = Song.new("hit me baby one more time", "Brittany Spears", "pop")
+
+p hit_me.name
+p hit_me.artist
