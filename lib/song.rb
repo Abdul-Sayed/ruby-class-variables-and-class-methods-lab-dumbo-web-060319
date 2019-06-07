5cc8d0f126326fcc32eabc7536b7721eb127157a
Abdul <awsayed@alaska.edu>
2019-06-07 15:19:14 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  @@genre_count = {}
+  @@artist_count = {}
+
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name, @artist = artist, @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+  
+end
